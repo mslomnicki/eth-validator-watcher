6fd4069212c1636878ad2d9377ae9e8ffe727206
Marek Słomnicki <marek@wildland.io>
2023-11-14 17:20:16 +0100

    Add sync committee rewards

--- eth_validator_watcher/sync_committee_reward.py
+++ eth_validator_watcher/sync_committee_reward.py
@@ -4,22 +4,29 @@ from prometheus_client import Counter
 from eth_validator_watcher.beacon import Beacon, NoBlockError
 from eth_validator_watcher.models import Validators
 
-our_sync_committee_reward_per_validator_count: Counter | None = None
+our_pos_sync_committee_reward_per_validator_count: Counter | None = None
+our_neg_sync_committee_reward_per_validator_count: Counter | None = None
 
 
 def init_sync_committee_reward_per_validator_counters(our_labels: dict[str, dict[str, str]]) -> None:
-    global our_sync_committee_reward_per_validator_count
+    global our_pos_sync_committee_reward_per_validator_count
+    global our_neg_sync_committee_reward_per_validator_count
 
-    if len(our_labels) == 0 or our_sync_committee_reward_per_validator_count is not None:
+    if len(our_labels) == 0 or our_pos_sync_committee_reward_per_validator_count is not None:
         return
 
     labels = list(random.choice(list(our_labels.values())).keys())
-    our_sync_committee_reward_per_validator_count = Counter(
-        "our_sync_committee_reward_per_validator_count",
-        "Our sync committee reward per validator counter",
+    our_pos_sync_committee_reward_per_validator_count = Counter(
+        "our_pos_sync_committee_reward_per_validator_count",
+        "Our positive sync committee reward per validator counter",
+        labels)
+    our_neg_sync_committee_reward_per_validator_count = Counter(
+        "our_neg_sync_committee_reward_per_validator_count",
+        "Our negative sync committee reward per validator counter",
         labels)
     for labels_dict in our_labels.values():
-        our_sync_committee_reward_per_validator_count.labels(**labels_dict)
+        our_pos_sync_committee_reward_per_validator_count.labels(**labels_dict)
+        our_neg_sync_committee_reward_per_validator_count.labels(**labels_dict)
 
 
 def process_sync_committee_reward(beacon: Beacon,
@@ -44,6 +51,9 @@ def process_sync_committee_reward(beacon: Beacon,
         for val in data:
             if val.validator_index in index_to_validator:
                 labels = our_labels[index_to_validator[val.validator_index].pubkey]
-                our_sync_committee_reward_per_validator_count.labels(**labels).inc(val.reward)
+                (our_pos_sync_committee_reward_per_validator_count
+                 if val.reward >= 0
+                 else our_neg_sync_committee_reward_per_validator_count
+                 ).labels(**labels).inc(abs(val.reward))
     except NoBlockError:
         pass
